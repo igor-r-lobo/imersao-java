ad4ede4aeb18e012d9a2614fec37baa28c514e80
igorlobo@ext.cvccorp.com.br <igorlobo@ext.cvccorp.com.br>
2023-03-27 20:34:15 -0300

    27-03 imersao java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import Services.ImdbService;
+import java.io.IOException;
+import java.util.ArrayList;
+import DAO.Movie;
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+	ImdbService imdbService = new ImdbService();
+	ArrayList<Movie> moviesList = imdbService.getMoviesList();
+
+	for (Movie movie: moviesList) {
+	   System.out.println(movie.getMovieData());
+	}
+    }
+}
---
+++ src/Services/ImdbService.java
@@ -0,0 +1,40 @@
+package Services;
+
+import Clients.ImdbClient;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import Utils.JsonParser;
+import DAO.Movie;
+
+public class ImdbService {
+    private String movies;
+    private List<Map<String,String>> moviesList;
+
+    public ImdbService() throws IOException, InterruptedException {
+	this.makeRequest();
+	this.parseData();
+    }
+
+    public ArrayList<Movie> getMoviesList() {
+	ArrayList<Movie> movieListData = new ArrayList<>();
+
+	for (Map<String,String> MovieList: this.moviesList) {
+	    Movie movie = new Movie(MovieList);
+	    movieListData.add(movie);
+	}
+
+	return movieListData;
+    }
+
+    private void makeRequest() throws IOException, InterruptedException {
+	ImdbClient client = new ImdbClient();
+	this.movies = client.getData();
+    }
+
+    private void parseData(){
+	JsonParser jsonParser = new JsonParser();
+	this.moviesList = jsonParser.parse(this.movies);
+    }
+}
